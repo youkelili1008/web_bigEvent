0e2e48117f3d6b5dfb5f98348fc44945bfe08b4b
youkelili <13432844130@163.com>
2022-05-27 18:17:48 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,26 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
-    console.log(options.url)
+    // console.log(options.url)
+
+    // 同一为有权限的接口，设置headers请求头
+    // 以 /my 开头的请求路径，需要在请求头中携带 Authorization 身份认证字段，才能正常访问成功
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载complete 回调函数
+    options.complete = function (res) {
+        // console.log('执行了 complete 回调函数');
+        // console.log(res);
+        // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据。
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 强制清空localStorge中的token
+            localStorage.removeItem('token');
+            // 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
